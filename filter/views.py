4358a4a4975fef126a6aa897ed9a5904d9086314
Elvis Claudino <102040112+elvisclaudino@users.noreply.github.com>
2023-10-23 22:09:24 -0300

    [add]: footer

--- filter/views.py
+++ filter/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from django.db import connection
+from django.http import HttpResponse
 
-from .models import Processos, Pessoas
-from django.db.models import Subquery, OuterRef
+from .models import Processos
 
 def processTable(request):
-    processos = Processos.objects.all()
+    lista_processos = Processos.objects.all().order_by("id")
+    paginator = Paginator(lista_processos, 10)
+    page = request.GET.get("page")
+    processos = paginator.get_page(page)
 
     return render(request, 'filter/processTable.html', {'processos': processos})
